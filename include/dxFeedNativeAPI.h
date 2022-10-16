415a9c039d8967c3ab008c604239002af4a3d9bf
Anatoly Kalin <ttldtor@gmail.com>
2022-10-16 23:02:10 +0300

    [EN-5236] [Multiplatform API (GraalVM)] Add system property processing

--- include/dxFeedNativeAPI.h
+++ include/dxFeedNativeAPI.h
@@ -3,9 +3,15 @@
 #ifndef DXFEED_NATIVE_API_H_INCLUDED
 #define DXFEED_NATIVE_API_H_INCLUDED
 
+#include <dxf_graal_error_codes.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+ERROR_CODE dxfc_system_set_property(const char *key, const char *value);
+
+ERROR_CODE dxfc_system_get_property(const char *key, char *value);
 
 #ifdef __cplusplus
 }
--- include/dxFeedNativeCppAPI.hpp
+++ include/dxFeedNativeCppAPI.hpp
@@ -2,4 +2,35 @@
 
 #pragma once
 
-namespace dxfcpp {}
+#include <dxf_graal_system.h>
+#include <dxf_graal_utils.h>
+
+#include <string>
+
+namespace dxfcpp {
+struct System {
+    static bool setProperty(const std::string& key, const std::string& value) {
+        graal_isolate_t *isolate;
+        graal_isolatethread_t *graalvmThread;
+        graal_create_isolate(nullptr, &isolate, &graalvmThread);
+
+        return dxf_graal_system_set_property(graalvmThread, key.c_str(), value.c_str()) == 0;
+    }
+
+    static std::string getProperty(const std::string& key) {
+        graal_isolate_t *isolate;
+        graal_isolatethread_t *graalvmThread;
+        graal_create_isolate(nullptr, &isolate, &graalvmThread);
+
+        std::string resultString{};
+
+        if (auto result = dxf_graal_system_get_property(graalvmThread, key.c_str())) {
+            resultString = result;
+            dxf_graal_utils_free(graalvmThread, reinterpret_cast<void*>(const_cast<char*>(result)));
+        }
+
+        return resultString;
+    }
+};
+
+} // namespace dxfcpp
