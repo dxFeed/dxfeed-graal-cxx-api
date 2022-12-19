c81fdce4d13d2b9d4dff797ea2e5af96025b5ced
ttldtor <ttldtor@gmail.com>
2022-12-19 15:30:42 +0300

    [EN-5233] [Multiplatform API (GraalVM)] Add endpoint entity

--- include/dxFeedNativeCppAPI.hpp
+++ include/dxFeedNativeCppAPI.hpp
@@ -6,6 +6,8 @@
 #include <dxfg_system.h>
 #include <graal_isolate.h>
 
+#include "internal/CEntryPointErrors.hpp"
+
 #include <string>
 
 #include <iostream>
@@ -21,118 +23,33 @@ namespace detail {
 using GraalIsolateHandle = std::add_pointer_t<graal_isolate_t>;
 using GraalIsolateThreadHandle = std::add_pointer_t<graal_isolatethread_t>;
 
-/**
- * Possible error codes returned by internal GraalVM functions
- *
- *  [Graal:CEntryPointErrors](https://github.com/oracle/graal/blob/96a1a66347bd4e5e00ae4e8e79812ebaf8cd5e33/substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/c/function/CEntryPointErrors.java#L43)
- */
-class CEntryPointErrors {
-    std::int32_t code_{};
-    std::string description_{};
-
-    CEntryPointErrors(std::int32_t code, std::string description) noexcept
-        : code_{code}, description_{std::move(description)} {}
-
-  public:
-    /// 0. No error occurred.
-    static const CEntryPointErrors NO_ERROR;
-
-    /// 1. An unspecified error occurred.
-    static const CEntryPointErrors UNSPECIFIED;
-
-    /// 2. An argument was NULL (nullptr).
-    static const CEntryPointErrors NULL_ARGUMENT;
-
-    /// 4. The specified thread is not attached to the isolate.
-    static const CEntryPointErrors UNATTACHED_THREAD;
-
-    /// 5. The specified isolate is unknown.
-    static const CEntryPointErrors UNINITIALIZED_ISOLATE;
-
-    /// 6. Locating the image file failed.
-    static const CEntryPointErrors LOCATE_IMAGE_FAILED;
-
-    /// 7. Opening the located image file failed.
-    static const CEntryPointErrors OPEN_IMAGE_FAILED;
-
-    /// 8. Mapping the heap from the image file into memory failed.
-    static const CEntryPointErrors MAP_HEAP_FAILED;
-
-    /// 801. Reserving address space for the new isolate failed.
-    static const CEntryPointErrors RESERVE_ADDRESS_SPACE_FAILED;
-
-    /// 802. The image heap does not fit in the available address space.
-    static const CEntryPointErrors INSUFFICIENT_ADDRESS_SPACE;
-
-    /// 9. Setting the protection of the heap memory failed.
-    static const CEntryPointErrors PROTECT_HEAP_FAILED;
-
-    /// 10. The version of the specified isolate parameters is unsupported.
-    static const CEntryPointErrors UNSUPPORTED_ISOLATE_PARAMETERS_VERSION;
-
-    /// 11. Initialization of threading in the isolate failed.
-    static const CEntryPointErrors THREADING_INITIALIZATION_FAILED;
-
-    /// 12. Some exception is not caught.
-    static const CEntryPointErrors UNCAUGHT_EXCEPTION;
-
-    /// 13. Initialization the isolate failed.
-    static const CEntryPointErrors ISOLATE_INITIALIZATION_FAILED;
-
-    /// 14. Opening the located auxiliary image file failed.
-    static const CEntryPointErrors OPEN_AUX_IMAGE_FAILED;
-
-    /// 15. Reading the opened auxiliary image file failed.
-    static const CEntryPointErrors READ_AUX_IMAGE_META_FAILED;
-
-    /// 16. Mapping the auxiliary image file into memory failed.
-    static const CEntryPointErrors MAP_AUX_IMAGE_FAILED;
-
-    /// 17. Insufficient memory for the auxiliary image.
-    static const CEntryPointErrors INSUFFICIENT_AUX_IMAGE_MEMORY;
-
-    /// 18. Auxiliary images are not supported on this platform or edition.
-    static const CEntryPointErrors AUX_IMAGE_UNSUPPORTED;
-
-    /// 19. Releasing the isolate's address space failed.
-    static const CEntryPointErrors FREE_ADDRESS_SPACE_FAILED;
-
-    /// 20. Releasing the isolate's image heap memory failed.
-    static const CEntryPointErrors FREE_IMAGE_HEAP_FAILED;
-
-    /// 21. The auxiliary image was built from a different primary image.
-    static const CEntryPointErrors AUX_IMAGE_PRIMARY_IMAGE_MISMATCH;
-
-    /// 22. The isolate arguments could not be parsed.
-    static const CEntryPointErrors ARGUMENT_PARSING_FAILED;
-
-    /// 23. Current target does not support the following CPU features that are required by the image.
-    static const CEntryPointErrors CPU_FEATURE_CHECK_FAILED;
-
-    static const std::unordered_map<std::int32_t, CEntryPointErrors> ALL;
-
-    static const CEntryPointErrors &valueOf(std::int32_t code) {
-        if (auto found = ALL.find(code); found != ALL.end()) {
-            return found->second;
-        }
-
-        return UNSPECIFIED;
-    }
-
-    /// Returns the code
-    [[nodiscard]] int32_t getCode() const { return code_; }
-
-    /// Returns the description
-    [[nodiscard]] const std::string &getDescription() const { return description_; }
-};
-
 class Isolate final {
-    struct IsolateThread final {
-        GraalIsolateThreadHandle graalIsolateThreadHandle_;
+    class IsolateThread final {
+        mutable std::mutex mutex_{};
+        GraalIsolateThreadHandle graalIsolateThreadHandle_{};
+
+      public:
+        IsolateThread() = default;
 
         CEntryPointErrors attach(const Isolate &isolate) {
+            std::lock_guard<std::mutex> guard{mutex_};
+
             return CEntryPointErrors::valueOf(graal_attach_thread(isolate.getHandleImpl(), &graalIsolateThreadHandle_));
         }
+
+        CEntryPointErrors detach() {
+            std::lock_guard<std::mutex> guard{mutex_};
+
+            if (graalIsolateThreadHandle_) {
+                auto result = CEntryPointErrors::valueOf(graal_detach_thread(graalIsolateThreadHandle_));
+
+                if (result == CEntryPointErrors::NO_ERROR) {
+
+                }
+            }
+        }
+
+        ~IsolateThread() { detach(); }
     };
 
     mutable std::shared_mutex mutex_{};
