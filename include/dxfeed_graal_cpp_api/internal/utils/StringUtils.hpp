c309b653421257dc3bd41911d9a4bde95ae03f0e
AnatolyKalin <anatoly.kalin@gmail.com>
2023-05-26 18:46:42 +0300

    [DXFC-402] Implement adding multiple symbols and removing symbols

--- include/dxfeed_graal_cpp_api/internal/utils/StringUtils.hpp
+++ include/dxfeed_graal_cpp_api/internal/utils/StringUtils.hpp
@@ -3,9 +3,9 @@
 
 #pragma once
 
+#include <cstdint>
 #include <string>
 #include <thread>
-#include <cstdint>
 
 namespace dxfcpp {
 
@@ -19,7 +19,23 @@ std::string toString(const char *chars);
 
 std::string toString(std::thread::id theadId);
 
-std::string toString(void* ptr);
+std::string toString(void *ptr);
+
+template <typename T> std::string toStringAny(T &&t) {
+    if constexpr (requires { toString(t); }) {
+        return toString(t);
+    } else if constexpr (requires { t.toString(); }) {
+        return t.toString();
+    } else if constexpr (requires { *t.toString(); }) {
+        return *t.toString();
+    } else if constexpr (requires { std::to_string(t); }) {
+        return std::to_string(t);
+    } else if constexpr (requires { std::string(t); }) {
+        return std::string(t);
+    } else {
+        return "unknown";
+    }
+}
 
 char utf16to8(std::int16_t in);
 
@@ -47,4 +63,4 @@ std::string encodeChar(std::int16_t c);
 
 inline std::string encodeChar(char c) { return encodeChar(static_cast<std::int16_t>(static_cast<unsigned char>(c))); }
 
-}
+} // namespace dxfcpp
