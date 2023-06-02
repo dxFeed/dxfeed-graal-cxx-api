896d2da597b2ff3148ba073cfc2ae284cf16d50a
AnatolyKalin <anatoly.kalin@gmail.com>
2023-06-02 14:37:05 +0300

    [DXFC-406] Implement API assembly in zip-bundle

--- tools/Tools/src/main.cpp
+++ tools/Tools/src/main.cpp
@@ -1,6 +1,8 @@
 // Copyright (c) 2023 Devexperts LLC.
 // SPDX-License-Identifier: MPL-2.0
 
-int main() {
+#include <dxfeed_graal_cpp_api/api.hpp>
 
+int main() {
+    dxfcpp::DXFeed::getInstance();
 }
