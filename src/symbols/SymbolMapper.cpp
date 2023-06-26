6b756cc145c4d332c42c3e8ba1ed6353dcbf0eea
AnatolyKalin <anatoly.kalin@gmail.com>
2023-06-26 17:58:21 +0300

    [EN-6893] Improve the work with Graal SDK lists

--- src/symbols/SymbolMapper.cpp
+++ src/symbols/SymbolMapper.cpp
@@ -10,9 +10,30 @@
 
 namespace dxfcpp {
 
-template <typename SymbolIt> void *SymbolMapper::toNativeList(SymbolIt /*begin*/, SymbolIt /*end*/) noexcept {
+template <typename SymbolIt> void *SymbolMapper::toNativeList(SymbolIt begin, SymbolIt end) noexcept {
+    using ListType = dxfg_symbol_list;
+    using SizeType = decltype(dxfg_symbol_list::size);
+    using ElelemntType = RemoveAllPointers<decltype(dxfg_symbol_list::elements)>;
 
-    return nullptr;
+    auto size = std::distance(begin, end);
+
+    if (size < 0) {
+        size = 0;
+    }
+
+    if (size > std::numeric_limits<SizeType>::max()) {
+        size = std::numeric_limits<SizeType>::max();
+    }
+
+    // Zero size is needed, for example, to clear the list of symbols.
+    ListType *list = new (std::nothrow) ListType{static_cast<SizeType>(size), nullptr};
+
+    if (!list) {
+        // TODO: error handling
+        return list;
+    }
+
+    return list;
 }
 
 } // namespace dxfcpp
