900684d4f22ea0993f26179a2be58efcde3ec57d
AnatolyKalin <anatoly.kalin@gmail.com>
2023-07-17 14:42:47 +0300

    [EN-7021] Add *Order events

--- include/dxfeed_graal_cpp_api/event/market/OrderSource.hpp
+++ include/dxfeed_graal_cpp_api/event/market/OrderSource.hpp
@@ -41,6 +41,319 @@ class DXFCPP_EXPORT OrderSource final : public IndexedEventSource {
     static std::uint32_t composeId(const std::string & /*name*/) noexcept { return {}; }
 
     void *toGraal() const noexcept override;
+
+  public:
+    /**
+     * Bid side of a composite Quote.
+     * It is a <em>synthetic</em> source.
+     * The subscription on composite Quote event is observed when this source is subscribed to.
+     */
+    static const OrderSource COMPOSITE_BID;
+
+    /**
+     * Ask side of a composite Quote.
+     * It is a <em>synthetic</em> source.
+     * The subscription on composite Quote event is observed when this source is subscribed to.
+     */
+    static const OrderSource COMPOSITE_ASK;
+
+    /**
+     * Bid side of a regional Quote.
+     * It is a <em>synthetic</em> source.
+     * The subscription on regional Quote event is observed when this source is subscribed to.
+     */
+    static const OrderSource REGIONAL_BID;
+
+    /**
+     * Ask side of a regional Quote.
+     * It is a <em>synthetic</em> source.
+     * The subscription on regional Quote event is observed when this source is subscribed to.
+     */
+    static const OrderSource REGIONAL_ASK;
+
+    /**
+     * Bid side of an aggregate order book (futures depth and NASDAQ Level II).
+     * This source cannot be directly published via dxFeed API, but otherwise it is fully operational.
+     */
+    static const OrderSource AGGREGATE_BID;
+
+    /**
+     * Ask side of an aggregate order book (futures depth and NASDAQ Level II).
+     * This source cannot be directly published via dxFeed API, but otherwise it is fully operational.
+     */
+    static const OrderSource AGGREGATE_ASK;
+
+    /**
+     * Default source for publishing custom order books.
+     * Order, AnalyticOrder and SpreadOrder events are @ref ::isPublishable() "publishable"
+     * on this source and the corresponding subscription can be observed via DXPublisher.
+     */
+    static const OrderSource DEFAULT;
+
+    /**
+     * NASDAQ Total View.
+     *
+     * Order events are @ref ::isPublishable() "publishable" on this source and the corresponding subscription can be
+     * observed via DXPublisher.
+     */
+    static const OrderSource NTV;
+
+    /**
+     * NASDAQ Total View. Record for price level book.
+     *
+     * Order events are @ref ::isPublishable() "publishable" on this source and the corresponding subscription can be
+     * observed via DXPublisher.
+     */
+    static const OrderSource ntv;
+
+    /**
+     * NASDAQ Futures Exchange.
+     *
+     * Order events are @ref ::isPublishable() "publishable" on this source and the corresponding subscription can be
+     * observed via DXPublisher.
+     */
+    static const OrderSource NFX;
+
+    /**
+     * NASDAQ eSpeed.
+     *
+     * Order events are @ref ::isPublishable() "publishable" on this source and the corresponding subscription can be
+     * observed via DXPublisher.
+     */
+    static const OrderSource ESPD;
+
+    /**
+     * NASDAQ Fixed Income.
+     *
+     * Order events are @ref ::isPublishable() "publishable" on this source and the corresponding subscription can be
+     * observed via DXPublisher.
+     */
+    static const OrderSource XNFI;
+
+    /**
+     * Intercontinental Exchange.
+     *
+     * Order events are @ref ::isPublishable() "publishable" on this source and the corresponding subscription can be
+     * observed via DXPublisher.
+     */
+    static const OrderSource ICE;
+
+    /**
+     * International Securities Exchange.
+     *
+     * Order and SpreadOrder events are @ref ::isPublishable() "publishable" on this source and the corresponding
+     * subscription can be observed via DXPublisher.
+     */
+    static const OrderSource ISE;
+
+    /**
+     * Direct-Edge EDGA Exchange.
+     *
+     * Order events are @ref ::isPublishable() "publishable" on this source and the corresponding subscription can be
+     * observed via DXPublisher.
+     */
+    static const OrderSource DEA;
+
+    /**
+     * Direct-Edge EDGX Exchange.
+     *
+     * Order events are @ref ::isPublishable() "publishable" on this source and the corresponding subscription can be
+     * observed via DXPublisher.
+     */
+    static const OrderSource DEX;
+
+    /**
+     * Direct-Edge EDGX Exchange. Record for price level book.
+     *
+     * Order events are @ref ::isPublishable() "publishable" on this source and the corresponding subscription can be
+     * observed via DXPublisher.
+     */
+
+    static const OrderSource dex;
+
+    /**
+     * Bats BYX Exchange.
+     *
+     * Order events are @ref ::isPublishable() "publishable" on this source and the corresponding subscription can be
+     * observed via DXPublisher.
+     */
+    static const OrderSource BYX;
+
+    /**
+     * Bats BZX Exchange.
+     *
+     * Order events are @ref ::isPublishable() "publishable" on this source and the corresponding subscription can be
+     * observed via DXPublisher.
+     */
+    static const OrderSource BZX;
+
+    /**
+     * Bats BZX Exchange. Record for price level book.
+     *
+     * Order events are @ref ::isPublishable() "publishable" on this source and the corresponding subscription can be
+     * observed via DXPublisher.
+     */
+    static const OrderSource bzx;
+
+    /**
+     * Bats Europe BXE Exchange.
+     *
+     * Order events are @ref ::isPublishable() "publishable" on this source and the corresponding subscription can be
+     * observed via DXPublisher.
+     */
+    static const OrderSource BATE;
+
+    /**
+     * Bats Europe CXE Exchange.
+     *
+     * Order events are @ref ::isPublishable() "publishable" on this source and the corresponding subscription can be
+     * observed via DXPublisher.
+     */
+    static const OrderSource CHIX;
+
+    /**
+     * Bats Europe DXE Exchange.
+     *
+     * Order events are @ref ::isPublishable() "publishable" on this source and the corresponding subscription can be
+     * observed via DXPublisher.
+     */
+    static const OrderSource CEUX;
+
+    /**
+     * Bats Europe TRF.
+     *
+     * Order events are @ref ::isPublishable() "publishable" on this source and the corresponding subscription can be
+     * observed via DXPublisher.
+     */
+    static const OrderSource BXTR;
+
+    /**
+     * Borsa Istanbul Exchange.
+     *
+     * Order events are @ref ::isPublishable() "publishable" on this source and the corresponding subscription can be
+     * observed via DXPublisher.
+     */
+    static const OrderSource IST;
+
+    /**
+     * Borsa Istanbul Exchange. Record for particular top 20 order book.
+     *
+     * Order events are @ref ::isPublishable() "publishable" on this source and the corresponding subscription can be
+     * observed via DXPublisher.
+     */
+    static const OrderSource BI20;
+
+    /**
+     * ABE (abe.io) exchange.
+     *
+     * Order events are @ref ::isPublishable() "publishable" on this source and the corresponding subscription can be
+     * observed via DXPublisher.
+     */
+    static const OrderSource ABE;
+
+    /**
+     * FAIR (FairX) exchange.
+     *
+     * Order events are @ref ::isPublishable() "publishable" on this source and the corresponding subscription can be
+     * observed via DXPublisher.
+     */
+    static const OrderSource FAIR;
+
+    /**
+     * CME Globex.
+     *
+     * Order and AnalyticOrder events are @ref ::isPublishable() "publishable" on this source and the corresponding
+     * subscription can be observed via DXPublisher.
+     */
+    static const OrderSource GLBX;
+
+    /**
+     * CME Globex. Record for price level book.
+     *
+     * Order events are @ref ::isPublishable() "publishable" on this source and the corresponding subscription can be
+     * observed via DXPublisher.
+     */
+    static const OrderSource glbx;
+
+    /**
+     * Eris Exchange group of companies.
+     *
+     * Order events are @ref ::isPublishable() "publishable" on this source and the corresponding subscription can be
+     * observed via DXPublisher.
+     */
+    static const OrderSource ERIS;
+
+    /**
+     * Eurex Exchange.
+     *
+     * Order events are @ref ::isPublishable() "publishable" on this source and the corresponding subscription can be
+     * observed via DXPublisher.
+     */
+    static const OrderSource XEUR;
+
+    /**
+     * Eurex Exchange. Record for price level book.
+     *
+     * Order events are @ref ::isPublishable() "publishable" on this source and the corresponding subscription can be
+     * observed via DXPublisher.
+     */
+    static const OrderSource xeur;
+
+    /**
+     * CBOE Futures Exchange.
+     *
+     * Order events are @ref ::isPublishable() "publishable" on this source and the corresponding subscription can be
+     * observed via DXPublisher.
+     */
+    static const OrderSource CFE;
+
+    /**
+     * CBOE Options C2 Exchange.
+     *
+     * Order events are @ref ::isPublishable() "publishable" on this source and the corresponding subscription can be
+     * observed via DXPublisher.
+     */
+    static const OrderSource C2OX;
+
+    /**
+     * Small Exchange.
+     *
+     * Order events are @ref ::isPublishable() "publishable" on this source and the corresponding subscription can be
+     * observed via DXPublisher.
+     */
+    static const OrderSource SMFE;
+
+    /**
+     * Small Exchange. Record for price level book.
+     *
+     * Order events are @ref ::isPublishable() "publishable" on this source and the corresponding subscription can be
+     * observed via DXPublisher.
+     */
+    static const OrderSource smfe;
+
+    /**
+     * Investors exchange. Record for price level book.
+     *
+     * Order events are @ref ::isPublishable() "publishable" on this source and the corresponding subscription can be
+     * observed via DXPublisher.
+     */
+    static const OrderSource iex;
+
+    /**
+     * Members Exchange.
+     *
+     * Order events are @ref ::isPublishable() "publishable" on this source and the corresponding subscription can be
+     * observed via DXPublisher.
+     */
+    static const OrderSource MEMX;
+
+    /**
+     * Members Exchange. Record for price level book.
+     *
+     * Order events are @ref ::isPublishable() "publishable" on this source and the corresponding subscription can be
+     * observed via DXPublisher.
+     */
+    static const OrderSource memx;
 };
 
 } // namespace dxfcpp
--- src/event/market/OrderSource.cpp
+++ src/event/market/OrderSource.cpp
@@ -8,6 +8,49 @@
 
 namespace dxfcpp {
 
+const OrderSource OrderSource::COMPOSITE_BID(1, "COMPOSITE_BID", 0);
+const OrderSource OrderSource::COMPOSITE_ASK(2, "COMPOSITE_ASK", 0);
+const OrderSource OrderSource::REGIONAL_BID(3, "REGIONAL_BID", 0);
+const OrderSource OrderSource::REGIONAL_ASK(4, "REGIONAL_ASK", 0);
+const OrderSource OrderSource::AGGREGATE_BID(5, "AGGREGATE_BID", 0);
+const OrderSource OrderSource::AGGREGATE_ASK(6, "AGGREGATE_ASK", 0);
+const OrderSource OrderSource::DEFAULT(0, "DEFAULT",
+                                       PUB_ORDER | PUB_ANALYTIC_ORDER | PUB_SPREAD_ORDER | FULL_ORDER_BOOK);
+
+const OrderSource OrderSource::NTV("NTV", PUB_ORDER | FULL_ORDER_BOOK);
+const OrderSource OrderSource::ntv("ntv", PUB_ORDER);
+const OrderSource OrderSource::NFX("NFX", PUB_ORDER);
+const OrderSource OrderSource::ESPD("ESPD", PUB_ORDER);
+const OrderSource OrderSource::XNFI("XNFI", PUB_ORDER);
+const OrderSource OrderSource::ICE("ICE", PUB_ORDER);
+const OrderSource OrderSource::ISE("ISE", PUB_ORDER | PUB_SPREAD_ORDER);
+const OrderSource OrderSource::DEA("DEA", PUB_ORDER);
+const OrderSource OrderSource::DEX("DEX", PUB_ORDER);
+const OrderSource OrderSource::dex("dex", PUB_ORDER);
+const OrderSource OrderSource::BYX("BYX", PUB_ORDER);
+const OrderSource OrderSource::BZX("BZX", PUB_ORDER);
+const OrderSource OrderSource::bzx("bzx", PUB_ORDER);
+const OrderSource OrderSource::BATE("BATE", PUB_ORDER);
+const OrderSource OrderSource::CHIX("CHIX", PUB_ORDER);
+const OrderSource OrderSource::CEUX("CEUX", PUB_ORDER);
+const OrderSource OrderSource::BXTR("BXTR", PUB_ORDER);
+const OrderSource OrderSource::IST("IST", PUB_ORDER);
+const OrderSource OrderSource::BI20("BI20", PUB_ORDER);
+const OrderSource OrderSource::ABE("ABE", PUB_ORDER);
+const OrderSource OrderSource::FAIR("FAIR", PUB_ORDER);
+const OrderSource OrderSource::GLBX("GLBX", PUB_ORDER | PUB_ANALYTIC_ORDER);
+const OrderSource OrderSource::glbx("glbx", PUB_ORDER);
+const OrderSource OrderSource::ERIS("ERIS", PUB_ORDER);
+const OrderSource OrderSource::XEUR("XEUR", PUB_ORDER);
+const OrderSource OrderSource::xeur("xeur", PUB_ORDER);
+const OrderSource OrderSource::CFE("CFE", PUB_ORDER);
+const OrderSource OrderSource::C2OX("C2OX", PUB_ORDER);
+const OrderSource OrderSource::SMFE("SMFE", PUB_ORDER);
+const OrderSource OrderSource::smfe("smfe", PUB_ORDER);
+const OrderSource OrderSource::iex("iex", PUB_ORDER);
+const OrderSource OrderSource::MEMX("MEMX", PUB_ORDER);
+const OrderSource OrderSource::memx("memx", PUB_ORDER);
+
 void *OrderSource::toGraal() const noexcept {
     auto *graalSource = new (std::nothrow)
         dxfg_indexed_event_source_t{ORDER_SOURCE, static_cast<std::int32_t>(id()), createCString(name())};
