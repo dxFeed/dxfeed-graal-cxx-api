d8499809af524c7d81a4c2601ea482cfc75ba58f
Anatoly Kalin <anatoly.kalin@devexperts.com>
2023-09-19 15:23:03 +0300

    Fix bash build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,26 @@
 #!/bin/sh
-BUILD_TYPE="Release"
-BUILD_DIR="$(pwd)/build-$BUILD_TYPE"
+CUR_DIR="$(pwd)"
+DIST_DIR="$(pwd)/dist"
+TAG="$(git describe --tags --abbrev=0)"
 
-if [ -d "$BUILD_DIR" ]; then rm -Rf "$BUILD_DIR"; fi &&
-  mkdir "$BUILD_DIR" &&
-  cmake -B "$BUILD_DIR" -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DDXFCXX_PACKAGE_SUFFIX=-$BUILD_TYPE &&
-  cmake --build "$BUILD_DIR" --config $BUILD_TYPE --parallel 8 &&
-  cd "$BUILD_DIR" &&
-  cpack -G ZIP -C $BUILD_TYPE --config ./dxFeedGraalCxxApiPackConfig.cmake
+echo "--- $TAG ---"
+
+if [ ! -d "$DIST_DIR" ]; then mkdir "$DIST_DIR"; fi
+
+set -- "Release" "Debug" "RelWithDebInfo" "MinSizeRel"
+
+for BUILD_TYPE in "$@"
+do
+  BUILD_DIR="$(pwd)/build-$BUILD_TYPE"
+
+  if [ -d "$BUILD_DIR" ]; then rm -Rf "$BUILD_DIR"; fi &&
+    echo "--- $BUILD_TYPE ---" &&
+    echo "--- Removing old bundles ---" && find "$DIST_DIR" -maxdepth 1 -name "$TAG*$BUILD_TYPE.zip" -print0 | xargs -0 rm -f &&
+    mkdir "$BUILD_DIR" &&
+    echo "--- Configuring CMake ---" && cmake -B "$BUILD_DIR" -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DDXFCXX_VERSION="$TAG" -DDXFCXX_PACKAGE_SUFFIX=-$BUILD_TYPE &&
+    echo "--- Building ---" && cmake --build "$BUILD_DIR" --config $BUILD_TYPE --parallel 8 &&
+    cd "$BUILD_DIR" &&
+    echo "--- Packing ---" && cpack -G ZIP -C $BUILD_TYPE --config ./dxFeedGraalCxxApiPackConfig.cmake &&
+    echo "--- Coping bundles ---" && find . -maxdepth 1 -name "*$BUILD_TYPE.zip" -print0 | xargs -0 cp -rf -t "$DIST_DIR" &&
+    cd "$CUR_DIR" || exit
+done
