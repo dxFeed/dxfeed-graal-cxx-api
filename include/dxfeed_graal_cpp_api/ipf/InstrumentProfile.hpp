d487d2b0cdb571ae8f864c380303500b19a0f5d9
ttldtor <ttldtor@gmail.com>
2023-09-30 23:34:23 +0300

    [EN-7577] Implement DXFeedIpfConnect sample

--- include/dxfeed_graal_cpp_api/ipf/InstrumentProfile.hpp
+++ include/dxfeed_graal_cpp_api/ipf/InstrumentProfile.hpp
@@ -3,21 +3,25 @@
 
 #pragma once
 
-#include "../internal/Conf.hpp"
 #include "../internal/Common.hpp"
+#include "../internal/Conf.hpp"
 
 #include <cstdint>
+#include <optional>
 #include <string>
+#include <type_traits>
+#include <unordered_map>
+#include <utility>
 #include <vector>
 
 namespace dxfcpp {
 
 /**
  * Represents basic profile information about market instrument.
- * Please see <a href="http://www.dxfeed.com/downloads/documentation/dxFeed_Instrument_Profile_Format.pdf">Instrument Profile Format documentation</a>
- * for complete description.
+ * Please see <a href="http://www.dxfeed.com/downloads/documentation/dxFeed_Instrument_Profile_Format.pdf">Instrument
+ * Profile Format documentation</a> for complete description.
  */
-class DXFCPP_EXPORT InstrumentProfile final {
+class DXFCPP_EXPORT InstrumentProfile final : public SharedEntity {
     struct Data {
         std::string type{};
         std::string symbol{};
@@ -50,10 +54,119 @@ class DXFCPP_EXPORT InstrumentProfile final {
         std::string settlementStyle{};
         std::string priceIncrements{};
         std::string tradingHours{};
-        std::vector<std::string> customFields{};
+        std::unordered_map<std::string, std::string> customFields{};
     };
 
+    Data data_{};
 
+    void fillData(void *graalNative) noexcept;
+    void fillGraalData(void *graalNative) const noexcept;
+    static void freeGraalData(void *graalNative) noexcept;
+
+    /**
+     * Returns custom field value with a specified name.
+     *
+     * @param name The name of custom field.
+     * @return The reference to custom field value with a specified name or std::nullopt
+     */
+    std::optional<std::reference_wrapper<const std::string>> getCustomField(const std::string &name) const noexcept {
+        if (!data_.customFields.contains(name)) {
+            return std::nullopt;
+        }
+
+        return std::cref(data_.customFields.at(name));
+    }
+
+    /**
+     * Changes custom field value with a specified name.
+     *
+     * @param name The name of custom field.
+     * @param value The custom field value.
+     */
+    void setCustomField(const std::string &name, const std::string &value) {
+        data_.customFields[name] = value;
+    }
+
+  public:
+    static std::shared_ptr<InstrumentProfile> fromGraal(void *graalNative) noexcept;
+
+    /**
+     * Allocates memory for the dxFeed Graal SDK structure (recursively if necessary).
+     * Fills the dxFeed Graal SDK structure's fields by the data of the current entity (recursively if necessary).
+     * Returns the pointer to the filled structure.
+     *
+     * @return The pointer to the filled dxFeed Graal SDK structure
+     */
+    void *toGraal() const noexcept;
+
+    /**
+     * Releases the memory occupied by the dxFeed Graal SDK structure (recursively if necessary).
+     *
+     * @param graalNative The pointer to the dxFeed Graal SDK structure.
+     */
+    static void freeGraal(void *graalNative) noexcept;
+
+    /// The alias to a type of shared pointer to the InstrumentProfile object
+    using Ptr = std::shared_ptr<InstrumentProfile>;
+
+    /// The alias to a type of unique pointer to the InstrumentProfile object
+    using Unique = std::unique_ptr<InstrumentProfile>;
+
+    /**
+     * Creates new instrument profile with default values.
+     */
+    InstrumentProfile() noexcept = default;
+
+    /**
+     * Returns type of instrument.
+     * It takes precedence in conflict cases with other fields.
+     * It is a mandatory field. It may not be empty.
+     * Example: "STOCK", "FUTURE", "OPTION".
+     *
+     * @return type of instrument.
+     */
+    const std::string &getType() const & noexcept {
+        return data_.type;
+    }
+
+    /**
+     * Changes type of instrument.
+     * It takes precedence in conflict cases with other fields.
+     * It is a mandatory field. It may not be empty.
+     * Example: "STOCK", "FUTURE", "OPTION".
+     *
+     * @param type The type of instrument.
+     */
+    void setType(const std::string &type) noexcept {
+        data_.type = type;
+    }
+
+    /**
+     * Changes type of instrument and returns the current instrument profile.
+     *
+     * @param type The type of instrument.
+     * @return The current instrument profile.
+     */
+    InstrumentProfile &withType(const std::string &type) noexcept {
+        InstrumentProfile::setType(type);
+
+        return *this;
+    }
+
+    /**
+     * Changes type of instrument and returns a shared pointer to the current instrument profile.
+     *
+     * @warning Please do not use this method unless the object was created with `std::shared_ptr<InstrumentProfile>(new
+     * Candle(...))` or `std::make_shared<InstrumentProfile>(...)`
+     *
+     * @param type The type of instrument.
+     * @return A shared pointer to the current instrument profile.
+     */
+    InstrumentProfile::Ptr withTypeShared(const std::string &type) noexcept {
+        InstrumentProfile::setType(type);
+
+        return shared_from_this()->sharedAs<InstrumentProfile>();
+    }
 };
 
-}
+} // namespace dxfcpp
--- src/ipf/InstrumentProfile.cpp
+++ src/ipf/InstrumentProfile.cpp
@@ -1,3 +1,41 @@
-//
-// Created by ttldt on 9/29/2023.
-//
+// Copyright (c) 2023 Devexperts LLC.
+// SPDX-License-Identifier: MPL-2.0
+
+#include <dxfg_api.h>
+
+#include <dxfeed_graal_c_api/api.h>
+#include <dxfeed_graal_cpp_api/api.hpp>
+
+#include <cstring>
+#include <memory>
+#include <utf8.h>
+#include <utility>
+
+#include <fmt/chrono.h>
+#include <fmt/format.h>
+#include <fmt/ostream.h>
+#include <fmt/std.h>
+
+namespace dxfcpp {
+
+void InstrumentProfile::fillData(void *graalNative) noexcept {
+}
+
+void InstrumentProfile::fillGraalData(void *graalNative) const noexcept {
+}
+
+void InstrumentProfile::freeGraalData(void *graalNative) noexcept {
+}
+
+std::shared_ptr<InstrumentProfile> InstrumentProfile::fromGraal(void *graalNative) noexcept {
+    return {};
+}
+
+void *InstrumentProfile::toGraal() const noexcept {
+    return nullptr;
+}
+
+void InstrumentProfile::freeGraal(void *graalNative) noexcept {
+}
+
+} // namespace dxfcpp
