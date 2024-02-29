e47aa80a1166fdd161f1100096fed0e978d2d97b
ttldtor <ttldtor@gmail.com>
2024-02-29 15:30:36 +0300

    GraalException

--- include/dxfeed_graal_cpp_api/native/GraalException.hpp
+++ include/dxfeed_graal_cpp_api/native/GraalException.hpp
@@ -7,8 +7,30 @@
 
 DXFCXX_DISABLE_MSC_WARNINGS_PUSH(4251)
 
+#include "../internal/CEntryPointErrors.hpp"
+
+#include <stdexcept>
+#include <string>
+
 namespace dxfcpp {
 
-}
+struct DXFCPP_EXPORT GraalException : public std::runtime_error {
+    explicit GraalException(CEntryPointErrorsEnum entryPointErrorsEnum)
+        : std::runtime_error(createMessage(entryPointErrorsEnum)) {
+    }
+
+    static inline std::string createMessage(CEntryPointErrorsEnum entryPointErrorsEnum) {
+        auto result = CEntryPointErrorsEnumToStr(entryPointErrorsEnum);
+
+        if (!result) {
+            return std::string("Unknown error with error code: ") +
+                   std::to_string(static_cast<std::size_t>(entryPointErrorsEnum));
+        }
+
+        return result;
+    }
+};
+
+} // namespace dxfcpp
 
 DXFCXX_DISABLE_MSC_WARNINGS_POP()
