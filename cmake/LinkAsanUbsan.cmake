3f6ee53a47cdaf7a66fe8fc54bfc596846acd9d5
Anatoly Kalin <anatoly.kalin@devexperts.com>
2025-03-24 17:37:58 +0300

    Refactor sanitizer configuration for better granularity
    Split ASAN and UBSAN configurations into separate options and functions. Updated build matrix and CMake logic to handle each sanitizer independently, improving flexibility and clarity.

--- cmake/LinkAsanUbsan.cmake
+++ cmake/LinkAsanUbsan.cmake
@@ -1,8 +1,14 @@
 # Copyright (c) 2025 Devexperts LLC.
 # SPDX-License-Identifier: MPL-2.0
 
-function(LinkAsanUbsan targetName)
-    target_compile_options(${targetName} PRIVATE "-fsanitize=address" "-fsanitize=undefined")
-    target_link_options(${targetName} PRIVATE "-fsanitize=address" "-fsanitize=undefined")
-    target_link_libraries(${targetName} PUBLIC asan ubsan)
+function(LinkAsan targetName)
+    target_compile_options(${targetName} PRIVATE "-fsanitize=address")
+    target_link_options(${targetName} PRIVATE "-fsanitize=address")
+    #target_link_libraries(${targetName} PUBLIC asan ubsan)
+endfunction()
+
+function(LinkUbsan targetName)
+    target_compile_options(${targetName} PRIVATE "-fsanitize=undefined")
+    target_link_options(${targetName} PRIVATE "-fsanitize=undefined")
+    #target_link_libraries(${targetName} PUBLIC asan ubsan)
 endfunction()
