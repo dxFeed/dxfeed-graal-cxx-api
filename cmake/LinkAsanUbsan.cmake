abce06fe71ab8b0d07ee2094c0048ad9acb9a326
Anatoly Kalin <anatoly.kalin@devexperts.com>
2025-03-24 19:22:37 +0300

    Add Windows support for AddressSanitizer and refine workflows
    Updated build workflows to exclude UBSAN on Windows and streamlined the ctest command. Added support for AddressSanitizer on Windows in CMake, including necessary compiler definitions, while removing unused VS-specific ASAN option.

--- cmake/LinkAsanUbsan.cmake
+++ cmake/LinkAsanUbsan.cmake
@@ -2,13 +2,27 @@
 # SPDX-License-Identifier: MPL-2.0
 
 function(LinkAsan targetName)
-    target_compile_options(${targetName} PRIVATE "-fsanitize=address")
-    target_link_options(${targetName} PRIVATE "-fsanitize=address")
-    #target_link_libraries(${targetName} PUBLIC asan ubsan)
+    if (WIN32)
+        target_compile_options(${targetName} PRIVATE "/fsanitize=address")
+        target_link_options(${targetName} PRIVATE "/fsanitize=address")
+
+        target_compile_definitions(${targetName}
+                PUBLIC
+                $<$<COMPILE_LANG_AND_ID:CXX,MSVC>:
+                _DISABLE_VECTOR_ANNOTATION
+                _DISABLE_STRING_ANNOTATION
+                >
+        )
+    else ()
+        target_compile_options(${targetName} PRIVATE "-fsanitize=address")
+        target_link_options(${targetName} PRIVATE "-fsanitize=address")
+    endif ()
 endfunction()
 
 function(LinkUbsan targetName)
-    target_compile_options(${targetName} PRIVATE "-fsanitize=undefined")
-    target_link_options(${targetName} PRIVATE "-fsanitize=undefined")
-    #target_link_libraries(${targetName} PUBLIC asan ubsan)
+    if (WIN32)
+    else ()
+        target_compile_options(${targetName} PRIVATE "-fsanitize=undefined")
+        target_link_options(${targetName} PRIVATE "-fsanitize=undefined")
+    endif ()
 endfunction()
