062f029e1cb4e5857d168b9f05dd80d0e8b162ad
AnatolyKalin <anatoly.kalin@devexperts.com>
2025-10-24 15:47:27 +0300

    Speeded up compilation

--- src/api/osub/ObservableSubscriptionChangeListener.cpp
+++ src/api/osub/ObservableSubscriptionChangeListener.cpp
@@ -1,9 +1,14 @@
 // Copyright (c) 2025 Devexperts LLC.
 // SPDX-License-Identifier: MPL-2.0
 
-#include <dxfg_api.h>
+#include "dxfeed_graal_cpp_api/api/osub/ObservableSubscriptionChangeListener.hpp"
 
-#include <dxfeed_graal_cpp_api/api.hpp>
+#include "dxfeed_graal_cpp_api/internal/context/ApiContext.hpp"
+#include "dxfeed_graal_cpp_api/internal/managers/EntityManager.hpp"
+
+#include "dxfeed_graal_cpp_api/isolated/api/osub/IsolatedObservableSubscriptionChangeListener.hpp"
+
+#include <dxfg_api.h>
 
 DXFCPP_BEGIN_NAMESPACE
 
