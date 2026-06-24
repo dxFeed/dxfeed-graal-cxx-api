ba7dcf784f2d22b5f3e46f11da1ef209e7162a49
AnatolyKalin <anatoly.kalin@devexperts.com>
2026-06-24 22:00:13 +0300

    v6.0.0

--- include/dxfeed_graal_cpp_api/internal/Conf.hpp
+++ include/dxfeed_graal_cpp_api/internal/Conf.hpp
@@ -93,7 +93,7 @@
 #ifndef DXFCPP_BEGIN_NAMESPACE
 #    define DXFCPP_BEGIN_NAMESPACE                                                                                     \
         namespace dxfcpp {                                                                                             \
-        inline namespace v5 {
+        inline namespace v6 {
 #    define DXFCPP_END_NAMESPACE                                                                                       \
         }                                                                                                              \
         }
